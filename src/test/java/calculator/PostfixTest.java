66801fdeb56dc43eaa5b7e81b5fbc045d81da5d3
Sam Phillips <samphillips997@gmail.com>
2025-01-19 19:56:30 -0800

    Add some postfix tests

--- src/test/java/calculator/PostfixTest.java
+++ src/test/java/calculator/PostfixTest.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostfixTest {
+    @Test
+    void testConvertToPostfix() {
+        Postfix postfix = new Postfix();
 
+        String postfixExpression = "2 2 10 10 * * + 2 /";
+        var expressionArray = Arrays.asList(postfixExpression.split(" "));
+
+        assertEquals(expressionArray, postfix.convertToPostfix("(2+2*(10*10))/2"));
+    }
+
+    @Test
+    void testCalculatePostfixComplex() {
+        Postfix postfix = new Postfix();
+        assertEquals(new BigInteger("101"), postfix.calculatePostfix("(2+2*(10*10))/2"));
+    }
 }
